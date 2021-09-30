6bd4547db070bdf8d0bdcfe8bb6fd0de0d401baf
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-09-30 13:57:34 +0200

    Add starting point for interacting with backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Editor from './Editor.js';
+import logo from "./logo.svg";
+import "./App.css";
+import Editor from "./Editor.js";
+import { useEffect, useState } from "react";
+
+function AllDocumentsList() {
+  const [allDocuments, setAllDocuments] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:1337/api/v1/")
+      .then((response) => response.json())
+      .then((result) => setAllDocuments(result.data));
+  }, []);
+
+  function handleClick(event, name) {
+    event.preventDefault();
+    console.log(event.target.innerText, name);
+  }
+
+  return (
+    <div>
+      {allDocuments.map(({ _id: id, name, html }) => (
+        <div
+          key={id}
+          // onClick={handleClick}
+          onClick={(event) => {
+            handleClick(event, html);
+          }}
+        >
+          {name}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <Editor />
+    <>
+      <AllDocumentsList />
+      <Editor />
+    </>
   );
 }
 
