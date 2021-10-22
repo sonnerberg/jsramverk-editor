09c97ae99634a93c07f1f17a7c2360158a8374b4
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-10-22 23:41:50 +0200

    Update socket endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import socketIOClient from 'socket.io-client';
 
-const ENDPOINT = 'http://127.0.0.1:1337';
+const ENDPOINT = process.env.NODE_ENV === 'production' ? 'https://jsramverk-editor-pene14.azurewebsites.net/' :  'http://127.0.0.1:1337';
+
 export const socket = socketIOClient(ENDPOINT);
